7f183209f7c01d4efa56283cfabcaab9bbbd2e91
Laurel Machak <laurelmachak@gmail.com>
2017-11-21 15:25:05 -0800

    made the grid into a function where you can specify number of rows and columns

--- grid.py
+++ grid.py
@@ -1,11 +1,14 @@
-grid = []
-for row in range (10):
-    grid.append([])
-    for column in range (10):
-        grid[row].append(0)
+def create_grid(rows=5,columns=10):
+    '''returns nesetd array with first dimension
+    number of rows, and second dimension number of
+    columns   '''
+    grid = []
+    for row in range(rows):
+        grid.append([])
+        for column in range(columns):
+            grid[row].append(0)
+    return grid
 
-#print(grid)
-
+grid = create_grid()
 for i in grid:
     print(i)
-    
